2f3134f9b187e8bd1db9521948e03f4148c61130
Michal Kuffa <michal.kuffa@gmail.com>
2016-10-22 10:29:36 +0200

    Rename send to notify in templating function

--- pg_bawler/core.py
+++ pg_bawler/core.py
@@ -54,13 +54,13 @@ class SenderMixin:
 
     NOTIFY_SEND_TPL = 'SELECT pg_notify(\'{channel}\', \'{payload}\')'
 
-    def get_send_statement(self, *, channel, payload):
+    def get_notify_statement(self, *, channel, payload):
         return self.NOTIFY_SEND_TPL.format(channel=channel, payload=payload)
 
     async def send(self, *, channel, payload):
         async with (await self.pg_connection()).cursor() as pg_cursor:
             await pg_cursor.execute(
-                self.get_send_statement(channel=channel, payload=payload))
+                self.get_notify_statement(channel=channel, payload=payload))
 
 
 class ListenerMixin:
