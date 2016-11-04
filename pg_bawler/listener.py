b2b5e91649cdfadda63e11dcfe5ef5c105d28f23
Michal Kuffa <michal.kuffa@gmail.com>
2016-11-04 23:09:00 +0100

    Add timeout cli arg

--- pg_bawler/listener.py
+++ pg_bawler/listener.py
@@ -40,12 +40,20 @@ def get_default_cli_args_parser():
     parser.add_argument(
         '--log-level',
         metavar='LOG_LEVEL',
-        default='info',
-        help='Log level. One of: fatal, cirtical, error, warning, info, debug')
+        default='INFO',
+        help='Log level. One of: FATAL, CIRTICAL, ERROR, WARNING, INFO, DEBUG')
     parser.add_argument(
         '--dsn',
         metavar='DSN',
+        required=True,
         help='Connection string. e.g. `dbname=test user=postgres`')
+    parser.add_argument(
+        '--timeout',
+        metavar='TIMEOUT', default=5, type=int,
+        help=(
+            'Timeout for getting notification.'
+            ' If this timeout passes pg_bawler checks'
+            ' connection if it\'s alive'))
     parser.add_argument(
         '--handler',
         metavar='HANDLER', default='pg_bawler.listener:default_handler',
@@ -75,17 +83,17 @@ class NotificationListener(
 
 
 def main(*argv):
-    args = get_default_cli_args_parser(argv or sys.argv[1:]).parse_args()
+    args = get_default_cli_args_parser().parse_args(argv or sys.argv[1:])
     try:
         logging.basicConfig(
             format='[%(asctime)s][%(name)s][%(levelname)s]: %(message)s',
-            level=args.log_level)
+            level=args.log_level.upper())
     except TypeError:
         sys.exit('Worng log level. --help for more info.')
     LOGGER.info('Starting pg_bawler listener for channel: %s', args.channel)
     loop = asyncio.get_event_loop()
     listener = NotificationListener(connection_params={'dsn': args.dsn})
-    listener.listen_timeout = 5
+    listener.listen_timeout = args.timeout
     listener.register_handler(resolve_handler(args.handler))
     loop.run_until_complete(listener.register_channel(args.channel))
     loop.run_until_complete(listener.listen())
