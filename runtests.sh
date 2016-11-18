c42bafd22a68dde9b0a0f53e374744946ea4e628
Michal Kuffa <michal.kuffa@gmail.com>
2016-11-18 23:30:29 +0100

    Update runtests with cov and pg-tag options
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 # pip install -Ue .
-pre-commit run --all-files && python -m pytest
+pre-commit run --all-files \
+  && python -m pytest \
+    -s \
+    --exitfirst \
+    --verbose \
+    --pg-tag 9.6 \
+    --cov pg_bawler \
+    --cov-report term-missing \
+    tests
