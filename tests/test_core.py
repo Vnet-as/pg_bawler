0f6c121a2bc5e126bceadd1bd4df1d5a15a7665d
Michal Kuffa <michal.kuffa@gmail.com>
2016-11-24 22:55:50 +0100

    Add test_core.py with 1st simple test
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

---
+++ tests/test_core.py
@@ -0,0 +1,9 @@
+
+import pytest
+import pg_bawler.core
+
+
+@pytest.mark.asyncio
+async def test_drop_connection_has_cache_attr():
+    base = pg_bawler.core.BawlerBase(None)
+    await base.drop_connection()
