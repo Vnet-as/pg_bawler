34ad4df311cb52cc2dca928d749143cd0d901250
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-04 22:25:50 +0100

    Read yaml configuration
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- pg_bawler/bawlerd/conf.py
+++ pg_bawler/bawlerd/conf.py
@@ -1,5 +1,7 @@
 import os
 
+import yaml
+
 #: Default name for bawlerd configuration file
 DEFAULT_CONFIG_FILENAME = 'pg_bawler.yml'
 
@@ -26,3 +28,32 @@ def build_config_location_list(
         os.path.abspath(os.path.join(os.path.expanduser(location), filename))
         for location in locations
     ]
+
+
+def _load_file(_file):
+    '''
+    Parse file into a python object (mapping).
+
+    TODO: Only yaml for now, maybe more formats later.
+    '''
+    return yaml.load(_file)
+
+
+def _merge_configs(base, precede):
+    '''
+    Merges configurations.
+
+    TODO: Merge different nested sections in different fashion.
+    '''
+    return {**base, **precede}
+
+
+def read_config_files(config_locations):
+    '''
+    Reads config files at ``locations`` and merge values.
+    '''
+    config = {}
+    for config_location in config_locations:
+        with open(config_location) as config_file:
+            config = _merge_configs(config, _load_file(config_file))
+    return config
