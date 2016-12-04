9f3abe5077fce0a2d7323a769fc063fca5b7aca8
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-04 23:13:55 +0100

    Add simple test for _load_file
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- tests/test_bawlerd.py
+++ tests/test_bawlerd.py
@@ -1,4 +1,6 @@
+import io
 import os
+from textwrap import dedent
 
 from pg_bawler import bawlerd
 
@@ -18,3 +20,22 @@ class TestBawlerdConfig:
 
         assert user_conf in bawlerd.conf.build_config_location_list()
         assert system_conf in bawlerd.conf.build_config_location_list()
+
+    def test__load_file(self):
+        config = bawlerd.conf._load_file(io.StringIO(dedent("""\
+            logging:
+              formatters:
+                standard:
+                  format: \"%(asctime)s %(levelname)s] %(name)s: %(message)s\"
+              handlers:
+                default:
+                  level: "INFO"
+                  formatter: standard
+                  class: logging.StreamHandler
+              loggers:
+                "":
+                  handlers: ["default"]
+                  level: INFO
+                  propagate: True
+        """)))
+        assert 'logging' in config
