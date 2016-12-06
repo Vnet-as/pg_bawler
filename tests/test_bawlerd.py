0fdb93fb73142315fe404b9a161ef19af0d920cd
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-06 23:16:14 +0100

    Add simple test for config builder
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- tests/test_bawlerd.py
+++ tests/test_bawlerd.py
@@ -39,3 +39,16 @@ class TestBawlerdConfig:
                   propagate: True
         """)))
         assert 'logging' in config
+
+    def test_read_config_files(self):
+        config_base = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), 'configs')
+        locations = [
+            os.path.join(config_base, 'etc'),
+            os.path.join(config_base, 'home'),
+        ]
+        config = bawlerd.conf.read_config_files(
+            bawlerd.conf.build_config_location_list(locations=locations))
+
+        assert config['common']['listen_timeout'] == 40
+        assert 'logging' in config
