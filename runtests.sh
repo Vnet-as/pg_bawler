e09a09fe2ed1d37c23b28b879e12e8f7b07c0570
Michal Kuffa <michal.kuffa@gmail.com>
2017-01-20 22:48:41 +0100

    Remove pre-commit call from runtestst.sh

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # pip install -Ue .
-pre-commit run --all-files \
-  && python -m pytest \
-    -s \
-    --exitfirst \
-    --verbose \
-    --pg-tag 9.6 \
-    --cov pg_bawler \
-    --cov-report term-missing \
-    tests
+python -m pytest \
+	-s \
+	--exitfirst \
+	--verbose \
+	--pg-tag 9.6 \
+	--cov pg_bawler \
+	--cov-report term-missing \
+	tests
